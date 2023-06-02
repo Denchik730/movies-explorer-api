041c4495c0b11ab36deaf46fd20ccc4cbfe74f33
Denis Gabbasov <gabbas.denn@mail.ru>
2023-06-02 17:42:17 +0300

    feat:задать валидацию паролю на минимум символов и максимум

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post(
     body: Joi.object().keys({
       email: Joi.string().required().email(),
       name: Joi.string().required(),
-      password: Joi.string().required(),
+      password: Joi.string().required().min(2).max(30),
     }),
   }),
   createUser,
@@ -25,7 +25,7 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required(),
+      password: Joi.string().required().min(2).max(30),
     }),
   }),
   login,
